aac0ac878ed8b72a3f33870f3d2324e970ff4e76
jean-siqueira <jean.siqueira@outlook.com.br>
2019-04-23 23:18:21 -0300

    interception http requests with angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataBase } from "./in-memory-database";
+
 @NgModule({
   declarations: [
     AppComponent
